a13d660828e60c8f31c94779fa747fab9c0d33af
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-12-10 06:44:02 +0530

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,6 +1,11 @@
+fix() {
+apt install --fix-broken
+}
+fix
 apt update
 apt upgrade -y
 apt install --flix-broken -y
 apt install wget python figlet toilet python-pip -y
+fix
 dpkg -i *deb
 cp font* termux* ~/.termux
