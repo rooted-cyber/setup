e1f95291d6862119724eacd2984b0a026666c589
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-02-07 09:31:43 +0530

    Add files via upload

--- set.sh
+++ set.sh
@@ -1,13 +1,61 @@
+fol() {
+cd ~/setup
+}
 fix() {
 apt install --fix-broken
 }
+oste() {
+cd /
+if [ -e system ];then
+printf "\033[1;92m Found Termux\n"
+fol
+dpkg -i a*term*deb
+cp font* termux* ~/.termux
+#cp .zshrc ~
+else
+printf "\033[1;91m Not Found Termux, any os found\n"
+fol
+dpkg -i a*os.deb
+fi
+}
+chp() {
+cd $PREFIX/bin
+if [ -e $a ];then
+printf "\n\033[1;93m Already installed $a\n"
+else
+printf "\n\033[1;95m Installing $a\n"
+apt install $a -y || apt reinstall $a -y
+fi
+}
+chps() {
+cd /bin
+if [ -e $a ];then
+printf "\n\033[1;93m Already installed $a\n"
+else
+printf "\n\033[1;95m Installing $a\n"
+apt install $a -y || apt reinstall $a -y
+fi
+}
+chtm() {
+cd /
+if [ -e system ];then
+printf "\033[1;92m Found Termux\n"
+chp
+else
+printf "\033[1;91m Not Found Termux, any os found\n"
+chps
+fi
+}
+sta() {
 fix
 apt update
 apt upgrade -y
 apt install --fix-broken -y
-apt install wget python figlet toilet python-pip ssh -y
+}
+sta
+for a in wget python3 figlet toilet python-pip openssh;do
+chtm
+done
+oste
+sta
 fix
-dpkg -i *deb
-cp font* termux* ~/.termux
-cp .zshrc ~
-chsh -s zsh
