4f4dc99b7e22ed6068abbad9d1c5cdf1e2132987
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-06-17 17:36:43 +0530

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,6 +1,6 @@
 dl() {
 cd ~
-wget -O https://gist.githubusercontent.com/rooted-cyber/05ca0b015cdbdb3ac87bb9cdaed3da75/
+wget -O tu https://gist.githubusercontent.com/rooted-cyber/05ca0b015cdbdb3ac87bb9cdaed3da75/raw/tu
 }
 fol() {
 cd ~/setup
