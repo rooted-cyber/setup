59a0013c8995374f6426b05411a0bc9e32bf5ede
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-09-15 15:14:32 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo -e "\033[1;92m"
 echo -e "$@" >&2
 }
 gc() {
+apt install --fix-broken -y
 cd ~
 if [ -e setup ];then
 msg Already clone
@@ -15,16 +16,28 @@ cd setup
 bash set.sh
 fi
 }
+pii() {
+echo
+msg installing $b
+sleep 1
+echo
+apt install $b -y
+}
 a() {
-printf "\n\033[1;92m Updating package..\n\n"
-apt install --fix-broken
-apt update
+msg starting....
+apt install --fix-broken -y
+msg update
 apt update -y
 apt upgrade -y
 apt-get update -y
 apt-get upgrade -y
-apt install --fix-broken
-apt install git python3 python-pip
-gc
+apt install --fix-broken -y
+for b in proot git wget figlet toilet python3 python-pip;do
+command -v $b  > /dev/null 2>&1 || pii
+done
+gc > /dev/null 2>&1
+cd ~
+wget -q -O .bashrc https://github.com/rooted-cyber/Zsh-files/raw/main/.bashrc
+apt install --fix-broken -y
 }
 a
