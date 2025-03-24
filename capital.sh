f197ccf023fa6d6a291b7f5a14899398d596450e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-03-24 09:33:24 +0530

    Add files via upload

---
+++ capital.sh
@@ -0,0 +1,89 @@
+cd $PREFIX/bin
+if [ -e random ];then
+		echo
+		else
+		printf "#!/data/data/com.termux/files/usr/bin/python3\nimport random\nimport os\ncolor=['\033[1;91m','\033[1;92m','\033[1;93m','\033[1;94m','\033[1;95m','\033[1;96m']\nprint(random.choice(color))\nexit()" >> $PREFIX/bin/random
+		chmod 777 $PREFIX/bin/random
+		fi
+random
+A="$(date +"%A")"
+B="$(date +"%B")"
+C="$(date +"%C")"
+D="$(date +"%D")"
+E="$(date +"%E")"
+F="$(date +"%F")"
+G="$(date +"%G")"
+H="$(date +"%H")"
+I="$(date +"%I")"
+J="$(date +"%J")"
+K="$(date +"%K")"
+L="$(date +"%L")"
+M="$(date +"%M")"
+N="$(date +"%N")"
+O="$(date +"%O")"
+P="$(date +"%P")"
+Q="$(date +"%Q")"
+R="$(date +"%R")"
+S="$(date +"%S")"
+T="$(date +"%T")"
+U="$(date +"%U")"
+V="$(date +"%V")"
+W="$(date +"%W")"
+X="$(date +"%X")"
+Y="$(date +"%Y")"
+Z="$(date +"%Z")"
+
+random
+printf "\n A :- \033[1;97m$A\n"
+random
+printf "\n B :- \033[1;97m$B\n"
+random
+printf "\n C :- \033[1;97m$C\n"
+random
+printf "\n D :- \033[1;97m$D\n"
+random
+printf "\n E :- \033[1;97m$E\n"
+random
+printf "\n F :- \033[1;97m$F\n"
+random
+printf "\n G :- \033[1;97m$G\n"
+random
+printf "\n H :- \033[1;97m$H\n"
+random
+printf "\n I :- \033[1;97m$I\n"
+random
+printf "\n J :- \033[1;97m$J\n"
+random
+printf "\n K :- \033[1;97m$K\n"
+random					
+printf "\n L :- \033[1;97m$L\n"
+random
+printf "\n M :- \033[1;97m$M\n"
+random
+printf "\n N :- \033[1;97m$N\n"
+random
+printf "\n O :- \033[1;97m$O\n"
+random
+printf "\n P :- \033[1;97m$P\n"
+random
+printf "\n Q :- \033[1;97m$Q\n"
+random
+printf "\n R :- \033[1;97m$R\n"
+random
+printf "\n S :- \033[1;97m$S\n"
+random
+printf "\n T :- \033[1;97m$T\n"
+random
+printf "\n U :- \033[1;97m$U\n"
+random
+printf "\n V :- \033[1;97m$V\n"
+random
+printf "\n W :- \033[1;97m$W\n"
+random
+printf "\n X :- \033[1;97m$X\n"
+random
+printf "\n Y :- \033[1;97m$Y\n"
+random
+printf "\n Z :- \033[1;97m$Z\n"
+
+
---
+++ small.sh
@@ -0,0 +1,91 @@
+cd $PREFIX/bin
+if [ -e random ];then
+		echo
+		else
+		printf "#!/data/data/com.termux/files/usr/bin/python3\nimport random\nimport os\ncolor=['\033[1;91m','\033[1;92m','\033[1;93m','\033[1;94m','\033[1;95m','\033[1;96m']\nprint(random.choice(color))\nexit()" >> $PREFIX/bin/random
+		chmod 777 $PREFIX/bin/random
+		fi
+		
+		
+random
+a="$(date +"%a")"
+b="$(date +"%b")"
+c="$(date +"%c")"
+d="$(date +"%d")"
+e="$(date +"%e")"
+f="$(date +"%f")"
+g="$(date +"%g")"
+h="$(date +"%h")"
+i="$(date +"%i")"
+j="$(date +"%j")"
+k="$(date +"%k")"
+l="$(date +"%l")"
+m="$(date +"%m")"
+n="$(date +"%n")"
+o="$(date +"%o")"
+p="$(date +"%p")"
+q="$(date +"%q")"
+r="$(date +"%r")"
+s="$(date +"%s")"
+t="$(date +"%t")"
+u="$(date +"%u")"
+v="$(date +"%v")"
+w="$(date +"%w")"
+x="$(date +"%x")"
+y="$(date +"%y")"
+z="$(date +"%z")"
+
+random
+printf "\n a :- \033[1;97m$a\n"
+random
+printf "\n b :- \033[1;97m$b\n"
+random
+printf "\n c :- \033[1;97m$c\n"
+random
+printf "\n d :- \033[1;97m$d\n"
+random
+printf "\n e :- \033[1;97m$e\n"
+random
+printf "\n f :- \033[1;97m$f\n"
+random
+printf "\n g :- \033[1;97m$g\n"
+random
+printf "\n h :- \033[1;97m$h\n"
+random
+printf "\n i :- \033[1;97m$i\n"
+random
+printf "\n j :- \033[1;97m$j\n"
+random
+printf "\n k :- \033[1;97m$k\n"
+random					
+printf "\n l :- \033[1;97m$l\n"
+random
+printf "\n m :- \033[1;97m$m\n"
+random
+printf "\n n :- \033[1;97m$n\n"
+random
+printf "\n o :- \033[1;97m$o\n"
+random
+printf "\n p :- \033[1;97m$p\n"
+random
+printf "\n q :- \033[1;97m$q\n"
+random
+printf "\n r :- \033[1;97m$r\n"
+random
+printf "\n s :- \033[1;97m$s\n"
+random
+printf "\n t :- \033[1;97m$t\n"
+random
+printf "\n u :- \033[1;97m$u\n"
+random
+printf "\n v :- \033[1;97m$v\n"
+random
+printf "\n w :- \033[1;97m$w\n"
+random
+printf "\n x :- \033[1;97m$x\n"
+random
+printf "\n y :- \033[1;97m$y\n"
+random
+printf "\n z :- \033[1;97m$z\n"
+
+
